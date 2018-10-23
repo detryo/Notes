82834bc5bf3bae9fbc30ab898c6ba64a2b35fac8
Cristian <chris.sedano.arenas@gmail.com>
2018-10-23 16:49:11 +0100

    PDFKit
    Implement Cocoa Touch Class, name FilesTablesViewController, Main.storyboard delete ViewController and implement Split View Controller and config the UI, import PDFs, in FilesTablesViewController.swift, create array, and config func numberOfSections, func tableView and func tableView, and implement the method didSelectRowAt, and config, change title, inside ViewController.swift create method func load

---
+++ Notes/Controllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Cristian Sedano Arenas on 21/10/18.
+//  Copyright © 2018 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func load(_ filename: String){
+        print(filename)
+    }
+
+}
+
